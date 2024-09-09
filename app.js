700653618e4988653f2348767a84d0fb32033fbe
xsm3z <xsm.zhang@gmail.com>
2024-09-09 15:22:54 -0400

    2-3 complete

--- app.js
+++ app.js
@@ -14,3 +14,35 @@ const nums2 = nums.map((currentElement) => {
 
 console.log(nums2)
 
+// Exercise 2
+
+// Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here
+
+const [firstTopping, secondTopping] = pizzaToppings;
+
+console.log(firstTopping);
+console.log(secondTopping)
+
+// Exercise 3
+
+// Given the following object, use destructuring to create variables `make` and `model` that will hold the respective values.
+
+const car = {
+  make: 'Audi',
+  model: 'q5',
+};
+
+// Your code here
+
+const { make, model } = car;
+
+console.log(make);
+console.log(model)
+
+// Exercise 4
+
+
